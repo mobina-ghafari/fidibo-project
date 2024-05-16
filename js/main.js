c9bea27005486b3c7105800803ba20b0ae05eab2
mobina-ghafari <135110634+mobina-ghafari@users.noreply.github.com>
2024-05-16 00:28:05 -0700

    js+buyinformation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,44 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let search = document.querySelector("#search");
     let searchFrequent = document.querySelector("#search-frequent");
+    let boxSeach = document.querySelector("#box-search");
+    let iconSearch = document.querySelector("#icon-search");
+    let buy = document.querySelector("#buy");
+    let buyInformation = document.querySelector("#information");
 
     search.addEventListener("click" , (e)=>{
         searchFrequent.style.display='block';
+        boxSeach.style.border='1px solid #4cd5dc';
+        boxSeach.style.backgroundColor='#212529';
+        iconSearch.style.color='#4cd5dc';
+        buyInformation.style.display='none';
+        e.stopPropagation();
+    });
+
+    buy.addEventListener("click" , (e)=>{
+        buyInformation.style.display='block';
+        searchFrequent.style.display='none';
+        boxSeach.style.border='1px solid #5c5f61';
+        boxSeach.style.backgroundColor='#040810';
+        iconSearch.style.color='#5c5f61';
         e.stopPropagation();
     });
 
     document.addEventListener("click" , ()=>{
         searchFrequent.style.display='none';
+        boxSeach.style.border='1px solid #5c5f61';
+        boxSeach.style.backgroundColor='#040810';
+        iconSearch.style.color='#5c5f61';
+        buyInformation.style.display='none';
     });
 
     searchFrequent.addEventListener("click", (e)=>{
         e.stopPropagation();
     });
+    buyInformation.addEventListener("click", (e)=>{
+        e.stopPropagation();
+    })
 });
+
+
+
